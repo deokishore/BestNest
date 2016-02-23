c51b917de07f7ec7e9c60e819d7be3c0f83c6579
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 12:52:08 +0530

    @Ankit :- Converted trinitynx microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/TrinityNxController.java
+++ src/main/scala/com/bestnest/portal/web/controller/TrinityNxController.java
@@ -20,61 +20,61 @@ public class TrinityNxController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("trinitynx/trinitynx");
+		return new ModelAndView("trinitynx");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("trinitynx/pricelist");
+		ModelAndView mv = new ModelAndView("trinitynx.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("trinitynx/specification");
+		ModelAndView mv = new ModelAndView("trinitynx.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("trinitynx/floorplan");
+		ModelAndView mv = new ModelAndView("trinitynx.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("trinitynx/siteplan");
+		ModelAndView mv = new ModelAndView("trinitynx.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("trinitynx/paymentplan");
+		ModelAndView mv = new ModelAndView("trinitynx.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("trinitynx/locationmap");
+		ModelAndView mv = new ModelAndView("trinitynx.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("trinitynx/about");
+		ModelAndView mv = new ModelAndView("trinitynx.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("trinitynx/contact");
+		ModelAndView mv = new ModelAndView("trinitynx.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("trinitynx/sitevisit");
+		ModelAndView mv = new ModelAndView("trinitynx.siteVisit");
 		return mv;
  	}
 	
