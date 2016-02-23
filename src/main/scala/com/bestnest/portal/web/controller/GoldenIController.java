1e9ded2e2ea0f4f1a82ab42e05c480d14494c85a
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 12:23:05 +0530

    @Ankit :- Converted goldeni microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/GoldenIController.java
+++ src/main/scala/com/bestnest/portal/web/controller/GoldenIController.java
@@ -20,61 +20,61 @@ public class GoldenIController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("goldeni/goldeni");
+		return new ModelAndView("goldeni");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("goldeni/pricelist");
+		ModelAndView mv = new ModelAndView("goldeni.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("goldeni/specification");
+		ModelAndView mv = new ModelAndView("goldeni.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("goldeni/floorplan");
+		ModelAndView mv = new ModelAndView("goldeni.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("goldeni/siteplan");
+		ModelAndView mv = new ModelAndView("goldeni.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("goldeni/paymentplan");
+		ModelAndView mv = new ModelAndView("goldeni.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("goldeni/locationmap");
+		ModelAndView mv = new ModelAndView("goldeni.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("goldeni/about");
+		ModelAndView mv = new ModelAndView("goldeni.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("goldeni/contact");
+		ModelAndView mv = new ModelAndView("goldeni.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("goldeni/sitevisit");
+		ModelAndView mv = new ModelAndView("goldeni.siteVisit");
 		return mv;
  	}
 	
