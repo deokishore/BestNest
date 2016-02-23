e6c80c25ee9430fb9eef9d9f25ef1ecfad8dbd64
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 11:56:01 +0530

    @Ankit :- Converted bandhan microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/AarzaSquare1Controller.java
+++ src/main/scala/com/bestnest/portal/web/controller/AarzaSquare1Controller.java
@@ -25,56 +25,56 @@ public class AarzaSquare1Controller {
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("priceList");
+		ModelAndView mv = new ModelAndView("aarzasquare1.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("specification");
+		ModelAndView mv = new ModelAndView("aarzasquare1.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("floorPlan");
+		ModelAndView mv = new ModelAndView("aarzasquare1.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("sitePlan");
+		ModelAndView mv = new ModelAndView("aarzasquare1.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("paymentPlan");
+		ModelAndView mv = new ModelAndView("aarzasquare1.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("locationMap");
+		ModelAndView mv = new ModelAndView("aarzasquare1.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("about");
+		ModelAndView mv = new ModelAndView("aarzasquare1.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("contact");
+		ModelAndView mv = new ModelAndView("aarzasquare1.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("aarzasquare1/sitevisit");
+		ModelAndView mv = new ModelAndView("aarzasquare1.siteVisit");
 		return mv;
  	}
 	
--- src/main/scala/com/bestnest/portal/web/controller/BandhanController.java
+++ src/main/scala/com/bestnest/portal/web/controller/BandhanController.java
@@ -20,61 +20,61 @@ public class BandhanController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("bandhan/bandhan");
+		return new ModelAndView("bandhan");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("bandhan/pricelist");
+		ModelAndView mv = new ModelAndView("bandhan.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("bandhan/specification");
+		ModelAndView mv = new ModelAndView("bandhan.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("bandhan/floorplan");
+		ModelAndView mv = new ModelAndView("bandhan.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("bandhan/siteplan");
+		ModelAndView mv = new ModelAndView("bandhan.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("bandhan/paymentplan");
+		ModelAndView mv = new ModelAndView("bandhan.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("bandhan/locationmap");
+		ModelAndView mv = new ModelAndView("bandhan.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("bandhan/about");
+		ModelAndView mv = new ModelAndView("bandhan.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("bandhan/contact");
+		ModelAndView mv = new ModelAndView("bandhan.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("bandhan/sitevisit");
+		ModelAndView mv = new ModelAndView("bandhan.siteVisit");
 		return mv;
  	}
 	
