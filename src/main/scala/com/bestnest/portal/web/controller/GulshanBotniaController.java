fb59c1d8bb0ae38ed1d3ef9db838814a2258b845
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 15:35:09 +0530

    @Ankit :- Converted gulshan-botnia-sector144-noida microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/GulshanBotniaController.java
+++ src/main/scala/com/bestnest/portal/web/controller/GulshanBotniaController.java
@@ -20,61 +20,61 @@ public class GulshanBotniaController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("gulshan-botnia-sector144-noida/gulshan-botnia-sector144-noida");
+		return new ModelAndView("gulshan-botnia-sector144-noida");
  	}
 	
 	@RequestMapping(value="/pricelist", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/pricelist");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/specification");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorplan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/floorplan");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteplan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/siteplan");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentplan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/paymentplan");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationmap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/locationmap");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/about");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/contact");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitevisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida/sitevisit");
+		ModelAndView mv = new ModelAndView("gulshan-botnia-sector144-noida.sitevisit");
 		return mv;
  	}
 	
