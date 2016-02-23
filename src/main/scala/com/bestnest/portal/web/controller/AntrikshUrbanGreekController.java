781d541a4e82a7be8dfa46b90f2719e0d41aac26
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 15:19:25 +0530

    @Ankit :- Converted antrikshurbangreek microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/AntrikshUrbanGreekController.java
+++ src/main/scala/com/bestnest/portal/web/controller/AntrikshUrbanGreekController.java
@@ -20,61 +20,61 @@ public class AntrikshUrbanGreekController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  antrikshurbangreek Project Microsite Called : ");
-		return new ModelAndView("antrikshurbangreek/antrikshurbangreek");
+		return new ModelAndView("antrikshurbangreek");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/pricelist");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/specification");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/floorplan");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/siteplan");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/paymentplan");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/locationmap");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/about");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/contact");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("antrikshurbangreek/sitevisit");
+		ModelAndView mv = new ModelAndView("antrikshurbangreek.siteVisit");
 		return mv;
  	}
 	
