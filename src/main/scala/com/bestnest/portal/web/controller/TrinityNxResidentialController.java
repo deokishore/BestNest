efad2ba07f88acba1fe19cd34e5829136ab0905d
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 14:07:14 +0530

    @Ankit :- Converted trinitynxresidential microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/TrinityNxResidentialController.java
+++ src/main/scala/com/bestnest/portal/web/controller/TrinityNxResidentialController.java
@@ -20,61 +20,61 @@ public class TrinityNxResidentialController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("trinitynxresidential/trinitynxresidential");
+		return new ModelAndView("trinitynxresidential");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/pricelist");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/specification");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/floorplan");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/siteplan");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/paymentplan");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/locationmap");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/about");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/contact");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("trinitynxresidential/sitevisit");
+		ModelAndView mv = new ModelAndView("trinitynxresidential.siteVisit");
 		return mv;
  	}
 	
