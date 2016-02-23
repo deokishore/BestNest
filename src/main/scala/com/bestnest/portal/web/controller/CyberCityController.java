e4a2b7efe3e79c6bf1df3dd9854d115817c6a52f
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 12:14:00 +0530

    @Ankit :- Converted cybercity microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/CyberCityController.java
+++ src/main/scala/com/bestnest/portal/web/controller/CyberCityController.java
@@ -20,61 +20,61 @@ public class CyberCityController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("cybercity/cybercity");
+		return new ModelAndView("cybercity");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("cybercity/pricelist");
+		ModelAndView mv = new ModelAndView("cybercity.pricelist");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("cybercity/specification");
+		ModelAndView mv = new ModelAndView("cybercity.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("cybercity/floorplan");
+		ModelAndView mv = new ModelAndView("cybercity.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("cybercity/siteplan");
+		ModelAndView mv = new ModelAndView("cybercity.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("cybercity/paymentplan");
+		ModelAndView mv = new ModelAndView("cybercity.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("cybercity/locationmap");
+		ModelAndView mv = new ModelAndView("cybercity.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("cybercity/about");
+		ModelAndView mv = new ModelAndView("cybercity.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("cybercity/contact");
+		ModelAndView mv = new ModelAndView("cybercity.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("cybercity/sitevisit");
+		ModelAndView mv = new ModelAndView("cybercity.siteVisit");
 		return mv;
  	}
 	
