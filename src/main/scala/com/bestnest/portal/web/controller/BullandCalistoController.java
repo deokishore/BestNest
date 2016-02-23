f7a1ddb45dca86aee86b0f94e1ce43ab0e17ab75
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 13:09:03 +0530

    @Ankit :- Converted bullandcalisto microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/BullandCalistoController.java
+++ src/main/scala/com/bestnest/portal/web/controller/BullandCalistoController.java
@@ -20,61 +20,61 @@ public class BullandCalistoController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("bullandcalisto/bullandcalisto");
+		return new ModelAndView("bullandcalisto");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/pricelist");
+		ModelAndView mv = new ModelAndView("bullandcalisto.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/specification");
+		ModelAndView mv = new ModelAndView("bullandcalisto.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/floorplan");
+		ModelAndView mv = new ModelAndView("bullandcalisto.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/siteplan");
+		ModelAndView mv = new ModelAndView("bullandcalisto.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/paymentplan");
+		ModelAndView mv = new ModelAndView("bullandcalisto.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/locationmap");
+		ModelAndView mv = new ModelAndView("bullandcalisto.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/about");
+		ModelAndView mv = new ModelAndView("bullandcalisto.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/contact");
+		ModelAndView mv = new ModelAndView("bullandcalisto.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("bullandcalisto/sitevisit");
+		ModelAndView mv = new ModelAndView("bullandcalisto.siteVisit");
 		return mv;
  	}
 	
