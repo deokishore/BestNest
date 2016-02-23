9518d211e25398de532463b6356dc54469037856
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 13:45:33 +0530

    @Ankit :- Converted europiapark microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/EuropiaParkController.java
+++ src/main/scala/com/bestnest/portal/web/controller/EuropiaParkController.java
@@ -20,61 +20,61 @@ public class EuropiaParkController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("europiapark/europiapark");
+		return new ModelAndView("europiapark");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("europiapark/pricelist");
+		ModelAndView mv = new ModelAndView("europiapark.pricelist");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("europiapark/specification");
+		ModelAndView mv = new ModelAndView("europiapark.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("europiapark/floorplan");
+		ModelAndView mv = new ModelAndView("europiapark.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("europiapark/siteplan");
+		ModelAndView mv = new ModelAndView("europiapark.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("europiapark/paymentplan");
+		ModelAndView mv = new ModelAndView("europiapark.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("europiapark/locationmap");
+		ModelAndView mv = new ModelAndView("europiapark.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("europiapark/about");
+		ModelAndView mv = new ModelAndView("europiapark.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("europiapark/contact");
+		ModelAndView mv = new ModelAndView("europiapark.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("europiapark/sitevisit");
+		ModelAndView mv = new ModelAndView("europiapark.sitevisit");
 		return mv;
  	}
 	
