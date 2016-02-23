b773e4cefa8e6af383bd0be082c5dcafff2f75c0
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 14:54:23 +0530

    @Ankit :- Converted acecity & Amaatra microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/AceCityController.java
+++ src/main/scala/com/bestnest/portal/web/controller/AceCityController.java
@@ -20,61 +20,61 @@ public class AceCityController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("acecity/acecity");
+		return new ModelAndView("acecity");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("acecity/pricelist");
+		ModelAndView mv = new ModelAndView("acecity.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("acecity/specification");
+		ModelAndView mv = new ModelAndView("acecity.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("acecity/floorplan");
+		ModelAndView mv = new ModelAndView("acecity.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("acecity/siteplan");
+		ModelAndView mv = new ModelAndView("acecity.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("acecity/paymentplan");
+		ModelAndView mv = new ModelAndView("acecity.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("acecity/locationmap");
+		ModelAndView mv = new ModelAndView("acecity.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("acecity/about");
+		ModelAndView mv = new ModelAndView("acecity.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("acecity/contact");
+		ModelAndView mv = new ModelAndView("acecity.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("acecity/sitevisit");
+		ModelAndView mv = new ModelAndView("acecity.siteVisit");
 		return mv;
  	}
 	
--- src/main/scala/com/bestnest/portal/web/controller/AmaatraHomesController.java
+++ src/main/scala/com/bestnest/portal/web/controller/AmaatraHomesController.java
@@ -20,61 +20,61 @@ public class AmaatraHomesController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("amaatrahomes/amaatrahomes");
+		return new ModelAndView("amaatrahomes");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/pricelist");
+		ModelAndView mv = new ModelAndView("amaatrahomes.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/specification");
+		ModelAndView mv = new ModelAndView("amaatrahomes.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/floorplan");
+		ModelAndView mv = new ModelAndView("amaatrahomes.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/siteplan");
+		ModelAndView mv = new ModelAndView("amaatrahomes.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/paymentplan");
+		ModelAndView mv = new ModelAndView("amaatrahomes.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/locationmap");
+		ModelAndView mv = new ModelAndView("amaatrahomes.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/about");
+		ModelAndView mv = new ModelAndView("amaatrahomes.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/contact");
+		ModelAndView mv = new ModelAndView("amaatrahomes.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("amaatrahomes/sitevisit");
+		ModelAndView mv = new ModelAndView("amaatrahomes.siteVisit");
 		return mv;
  	}
 	
