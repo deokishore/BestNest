0a3543afd0638dec435c1ed94980eaf116c0cccc
Ankit <ankit.prajapati@tridenttechnologies.co.uk>
2016-02-23 13:40:47 +0530

    @Ankit :- Converted riseorganicghar microsite into tiles

--- src/main/scala/com/bestnest/portal/web/controller/RiseOrganicGharController.java
+++ src/main/scala/com/bestnest/portal/web/controller/RiseOrganicGharController.java
@@ -20,61 +20,61 @@ public class RiseOrganicGharController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView mahagunProject(HttpServletRequest request) {
 		logger.info("  Mahagun Project Microsite Called : ");
-		return new ModelAndView("riseorganicghar/riseorganicghar");
+		return new ModelAndView("riseorganicghar");
  	}
 	
 	@RequestMapping(value="/priceList", method = RequestMethod.GET)
 	public ModelAndView priceList() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/pricelist");
+		ModelAndView mv = new ModelAndView("riseorganicghar.priceList");
 		return mv;
  	}
 	
 	@RequestMapping(value="/specification", method = RequestMethod.GET)
 	public ModelAndView specification() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/specification");
+		ModelAndView mv = new ModelAndView("riseorganicghar.specification");
 		return mv;
  	}
 	
 	@RequestMapping(value="/floorPlan", method = RequestMethod.GET)
 	public ModelAndView floorPlan() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/floorplan");
+		ModelAndView mv = new ModelAndView("riseorganicghar.floorPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/sitePlan", method = RequestMethod.GET)
 	public ModelAndView sitePlan() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/siteplan");
+		ModelAndView mv = new ModelAndView("riseorganicghar.sitePlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/paymentPlan", method = RequestMethod.GET)
 	public ModelAndView paymentPlan() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/paymentplan");
+		ModelAndView mv = new ModelAndView("riseorganicghar.paymentPlan");
 		return mv;
  	}
 	
 	@RequestMapping(value="/locationMap", method = RequestMethod.GET)
 	public ModelAndView locationMap() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/locationmap");
+		ModelAndView mv = new ModelAndView("riseorganicghar.locationMap");
 		return mv;
  	}
 	
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
 	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/about");
+		ModelAndView mv = new ModelAndView("riseorganicghar.about");
 		return mv;
  	}
 	
 	@RequestMapping(value="/contact", method = RequestMethod.GET)
 	public ModelAndView contact() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/contact");
+		ModelAndView mv = new ModelAndView("riseorganicghar.contact");
 		return mv;
  	}
 	
 	@RequestMapping(value="/siteVisit", method = RequestMethod.GET)
 	public ModelAndView siteVisit() {
-		ModelAndView mv = new ModelAndView("riseorganicghar/sitevisit");
+		ModelAndView mv = new ModelAndView("riseorganicghar.siteVisit");
 		return mv;
  	}
 	
